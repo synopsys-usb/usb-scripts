61b0649e2a885fa6983521507ffde8c1794004be
Thinh Nguyen <thinhn@synopsys.com>
2018-07-12 14:48:22 -0700

    dwc3: audio: Fix audio IN direction
    Add the option 'record' to record audio IN direction from the host side.
    Previously the script only supports audio OUT.
    
    Option 'listen' will no longer save Audio OUT to a file. It will only
    play out the speaker or take the audio stream from a mic. It can still
    take an audio file for Audio IN direction.
    
    Change-Id: I620019835d420d9a1611458e5a6509f4486983ba
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -10,16 +10,18 @@ __complete_dwc3()
     if [ $COMP_CWORD -eq 1 ]; then
         COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 completion` )
     elif [ $COMP_CWORD -eq 2 ]; then
-        local arg=${COMP_WORDS[COMP_CWORD - 1]}
+        local arg=${COMP_WORDS[1]}
         if [ "$arg" = "load" ] || [ "$arg" = "audio" ]; then
             COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 $arg completion` )
         fi
-    elif [ $COMP_CWORD -eq 3 ]; then
-        local arg1=${COMP_WORDS[COMP_CWORD - 2]}
-        local arg2=${COMP_WORDS[COMP_CWORD - 1]}
+    elif [ $COMP_CWORD -ge 3 ]; then
+        local arg1=${COMP_WORDS[1]}
+        local arg2=${COMP_WORDS[2]}
         if [ "$arg1" = "audio" ] &&
-            [ "$arg2" = "play" ] || [ "$arg2" = "listen" ]; then
-            COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 $arg1 list_completion` )
+            [ "$arg2" = "play" ] ||
+            [ "$arg2" = "record" ] ||
+            [ "$arg2" = "listen" ]; then
+            COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 $arg1 audio_completion` )
         fi
     fi
 }
