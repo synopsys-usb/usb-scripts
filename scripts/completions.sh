15fd7859206ea6501866d08973fa54307c53b3dd
Thinh Nguyen <thinhn@synopsys.com>
2018-07-13 17:30:00 -0700

    lib: Add 'audio' to its own script
    Allow user to run 'audio' script without dwcX prefix.
    e.g. Now it's possible to do
     $ audio listen in
    
    Change-Id: I1a2de91bcac61caef571eab3e1c52bd9564b9bb0
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+__complete_audio()
+{
+    local arg=${COMP_WORDS[COMP_CWORD - 1]}
+
+    if [ "$arg" = "audio" ]; then
+        COMPREPLY=( `echo -n "$COMP_LINE" | audio completion` )
+    else
+        COMPREPLY=( `echo -n "$COMP_LINE" | audio audio_completion` )
+    fi
+}
+
 __complete_dwc2()
 {
     COMPREPLY=( `echo -n "$COMP_LINE" | dwc2 completion` )
@@ -7,21 +18,17 @@ __complete_dwc2()
 
 __complete_dwc3()
 {
+    local arg=${COMP_WORDS[1]}
+
     if [ $COMP_CWORD -eq 1 ]; then
         COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 completion` )
     elif [ $COMP_CWORD -eq 2 ]; then
-        local arg=${COMP_WORDS[1]}
         if [ "$arg" = "load" ] || [ "$arg" = "audio" ]; then
             COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 $arg completion` )
         fi
     elif [ $COMP_CWORD -ge 3 ]; then
-        local arg1=${COMP_WORDS[1]}
-        local arg2=${COMP_WORDS[2]}
-        if [ "$arg1" = "audio" ] && {
-            [ "$arg2" = "play" ] ||
-            [ "$arg2" = "record" ] ||
-            [ "$arg2" = "listen" ]; }; then
-            COMPREPLY=( `echo -n "$COMP_LINE" | dwc3 $arg1 audio_completion` )
+        if [ "$arg" = "audio" ]; then
+            __complete_audio
         fi
     fi
 }
@@ -31,6 +38,7 @@ __complete_dwc3_xhci()
     COMPREPLY=( `echo -n "$COMP_LINE" | dwc3-xhci completion` )
 }
 
+complete -F __complete_audio audio
 complete -F __complete_dwc2 dwc2
 complete -F __complete_dwc3 dwc3
 complete -F __complete_dwc3_xhci dwc3-xhci
