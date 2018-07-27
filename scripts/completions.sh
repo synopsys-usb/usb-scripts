85804752b14093f5852f9c5b03275201bba91452
Thinh Nguyen <thinhn@synopsys.com>
2018-07-26 17:42:12 -0700

    typec: Add auto-completion
    Add auto-completion for typec scripts.
    
    Change-Id: Ia96a2d83da425af6446d77f7a06b9b9f8af3ad71
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -22,6 +22,17 @@ __complete_audio()
     fi
 }
 
+__complete_typec()
+{
+    local arg=${COMP_WORDS[1]}
+
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( `echo -n "$COMP_LINE" | typec completion` )
+    elif [ $COMP_CWORD -eq 2 ] && [ "$arg" = "i2c" ]; then
+        COMPREPLY=( `echo -n "$COMP_LINE" | typec $arg completion` )
+    fi
+}
+
 __complete_dwc2()
 {
     COMPREPLY=( `echo -n "$COMP_LINE" | dwc2 completion` )
@@ -51,6 +62,7 @@ __complete_dwc3_xhci()
 
 complete -F __complete_haps haps
 complete -F __complete_audio audio
+complete -F __complete_typec typec
 complete -F __complete_dwc2 dwc2
 complete -F __complete_dwc3 dwc3
 complete -F __complete_dwc3_xhci dwc3-xhci
